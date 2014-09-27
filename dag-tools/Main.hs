b8eac448f6cea81931afb1047f431eabc67e2f90
Max Taldykin <jorpic@gmail.com>
2014-09-27 11:40:42 +0400

    dag-tools fix-inputs

--- dag-tools/Main.hs
+++ dag-tools/Main.hs
@@ -6,20 +6,20 @@ import Data.IntMap (IntMap)
 import qualified Data.IntMap as Map
 import System.Environment (getArgs)
 
--- FIXME: reverse DAG
 
 main :: IO ()
 main = do
-  dag <- map read . lines <$> getContents
   getArgs >>= \case
-    ["eval-check", inBits, outBits] -> do
+    ["eval-check", inBits, outBits] ->
       print =<< evalCheck
         <$> (parseBits <$> readFile inBits)
         <*> (parseBits <$> readFile outBits)
-        <*> pure dag
+        <*> (map read . lines <$> getContents)
     ["eval-trace", inBits] -> return () -- print bits from evalCheck
-    ["fix-inputs", inBits] -> do
-      mapM_ print =<< evalCheck
+    ["fix-inputs", inBits] ->
+      mapM_ print =<< fixInputs
+        <$> (parseBits <$> readFile inBits)
+        <*> (map read . lines <$> getContents)
     ["fix-outputs", outBits] -> return ()
     ["to-system"] -> return ()
     _ -> error
@@ -33,36 +33,57 @@ main = do
 
 
 evalCheck :: IntMap Bool -> IntMap Bool -> Dag -> Bool
-evalCheck inBits outBits = loop Map.empty . reverse
+evalCheck inBits outBits = loop Map.empty
   where
-    loop bits [] = True
+    loop _ [] = True
     loop bits ((ref,ex):exs) = case ex of
       Inp i -> case Map.lookup i inBits of
-        Nothing -> error $ "Input bit not found " ++ show i
+        Nothing -> error $ "Input is not fixed " ++ show i
         Just v  -> loop (Map.insert ref v bits) exs
-      Out o r -> case Map.lookup o outBits of
-        Nothing -> error $ "Output bit not found " ++ show o
-        Just a  -> case Map.lookup r bits of
-          Nothing -> error $ "Inconsistent DAG. Unevaluated ref " ++ show r
-          Just b
-            | a == b -> loop bits exs
-            | otherwise -> error $ "Check failed on output bit " ++ show r
+      Out o poly ->
+        case Map.lookup o outBits of
+        Nothing -> error $ "Output is not fixed " ++ show o
+        Just a
+          | a == evalBool bits poly -> loop bits exs
+          | otherwise -> error $ "Output bit mismatch " ++ show o
       Node poly ->
         let v = evalBool bits poly
-        in loop (Map.insert ref v bits) exs
+        in  loop (Map.insert ref v bits) exs
+
+
+
+fixInputs :: IntMap Bool -> Dag -> Dag
+fixInputs inBits = loop Map.empty
+  where
+    loop _ [] = []
+    loop inlMap ((ref,ex):exs) = case ex of
+      Inp i -> case Map.lookup i inBits of
+        Nothing -> (ref,ex) : loop inlMap exs
+        Just v  ->
+          let v' = if v then [[]] else []
+          in  loop (Map.insert ref v' inlMap) exs
+      Out o poly -> (ref, Out o $ inline inlMap poly) : loop inlMap exs
+      Node poly -> case inline inlMap poly of
+        -- inline constants, aliases, negations
+        []       -> loop (Map.insert ref [] inlMap) exs
+        [[]]     -> loop (Map.insert ref [[]] inlMap) exs
+        [[x]]    -> loop (Map.insert ref [[x]] inlMap) exs
+        [[],[x]] -> loop (Map.insert ref [[],[x]] inlMap) exs
+        poly'    -> (ref, Node poly') : loop inlMap exs
 
 
 type Ref = Int
+type Poly = [[Ref]]
 data Exp
   = Inp Int
-  | Out Int Ref
-  | Node [[Ref]]
+  | Out Int Poly
+  | Node Poly
   deriving (Show, Read)
 
 type Dag = [(Ref,Exp)]
 
 
-evalBool :: IntMap Bool -> [[Ref]] -> Bool
+evalBool :: IntMap Bool -> Poly -> Bool
 evalBool bits = evalXor
   where
     evalXor = odd . length . filter evalAnd
@@ -70,6 +91,22 @@ evalBool bits = evalXor
     value r = case Map.lookup r bits of
       Nothing -> error $ "Inconsistent DAG. Unevaluated ref " ++ show r
       Just v  -> v
+
+
+inline :: IntMap Poly -> Poly -> Poly
+inline inlMap = simpl . evalXor
+  where
+    evalXor = concatMap (evalAnd [])
+    evalAnd res [] = [res]
+    evalAnd res (x:xs) = case Map.lookup x inlMap of
+        Nothing -> evalAnd (x:res) xs
+        Just ys -> evalXor $ map (++res++xs) ys
+
+
+simpl :: Poly -> Poly
+simpl
+  = map head . filter (odd . length) . group . sort
+  . map (nub . sort)
 
 
 parseBits :: String -> IntMap Bool
@@ -82,13 +119,3 @@ parseBits
   . zip [0..]
   . concat . concatMap words
   . lines
-
-
-simpl :: [[Ref]] -> [[Ref]]
-simpl
-  = map head . filter (odd . length) . group . sort
-  . map (nub . sort)
-
-
-
-
